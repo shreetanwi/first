a357c041e05b11a9f5fd5e0d05260862737be820
shreetanwi <tanwishree26@gmail.com>
2019-03-10 12:59:24 +0530

    deed Car.go
    Added car.go
    
    

--- car.go
+++ car.go
@@ -5,30 +5,29 @@ import (
 )
 
 type car struct {
-	engine string,
-	colour string,
-	sunroof bool,
+	engine    string
+	colour    string
+	sunroof   bool
 	carnumber string
-
 }
 
 func main() {
 	fmt.Print("ho gaya")
 
-	alto := car {
-		engine : "four stroke",
-		colour : "white",
-		sunroof : false,
-		carnumber " JA 007 4R "
+	alto := car{
+		engine:    "four stroke",
+		colour:    "white",
+		sunroof:   false,
+		carnumber: " JA 007 4R ",
 	}
 
-	baleno := car {
-		engine : "four stroke",
-		colour : "grey",
-		sunroof : false,
-		carnumber " UP 007 4R ",
+	baleno := car{
+		engine:    "four stroke",
+		colour:    "grey",
+		sunroof:   false,
+		carnumber: " UP 007 4R ",
 	}
-	fmt.Print("ho gaya", alto)
-	fmt.Print("ho gaya", baleno)
+	fmt.Print("\n Sanjeev bhai ", alto)
+	fmt.Print("\n Shashi Shankar Pd", baleno)
 
 }
